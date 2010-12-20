d51ce4c3897175aa30c68df609d35cdd9ae8ca14
Bastian Pawlik <qwasyx.github.20.cooper@neverbox.com>
2010-12-20 14:52:02 +0100

    sidebar in details umbenannt. ID's der tickets verkuerzt. Events verlinkt beim Klick auf ID

--- evently/tickets/_changes/data.js
+++ evently/tickets/_changes/data.js
@@ -5,6 +5,7 @@ function(data) {
     tickets : data.rows.map(function(r) {
       p = (r.value && r.value.profile) || {};
       p.id = r.value._id;
+      p.idShort = r.value._id.substring(r.value._id.length-4,r.value._id.length);
       p.subject = r.value.subject;
       p.status = r.value.status;
       p.severity = r.value.severity;
